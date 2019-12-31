baed1238b45f98e8f5e9312b4af7eb3825e12a80
angujo <angujomondi@gmail.com>
2019-12-31 10:52:51 +0300

    added geotrace and geoshape

--- src/Models/Input.php
+++ src/Models/Input.php
@@ -5,11 +5,18 @@ namespace Angujo\OpenRosaPhp\Models;
 
 
 use Angujo\OpenRosaPhp\Core\ControlElement;
+use Angujo\OpenRosaPhp\Core\OException;
 
 class Input extends ControlElement
 {
-    public function __construct($name) { parent::__construct('input', $name); }
+    public function __construct($name){ parent::__construct('input', $name); }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function Text($name)
     {
         $me = new self($name);
@@ -17,6 +24,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function TextArea($name)
     {
         $me = new self($name);
@@ -25,6 +38,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function NumberInteger($name)
     {
         $me = new self($name);
@@ -32,6 +51,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function NumberDecimal($name)
     {
         $me = new self($name);
@@ -39,6 +64,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function BooleanType($name)
     {
         $me = new self($name);
@@ -46,6 +77,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function DateType($name)
     {
         $me = new self($name);
@@ -53,6 +90,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function DateMonthYear($name)
     {
         $me = new self($name);
@@ -60,6 +103,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function DateYear($name)
     {
         $me = new self($name);
@@ -67,6 +116,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function TimeType($name)
     {
         $me = new self($name);
@@ -74,6 +129,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function DateTime($name)
     {
         $me = new self($name);
@@ -81,6 +142,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function Barcode($name)
     {
         $me = new self($name);
@@ -88,6 +155,12 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function Url($name)
     {
         $me = new self($name);
@@ -95,12 +168,25 @@ class Input extends ControlElement
         return $me;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function Geopoint($name)
     {
         $me = new self($name);
         $me->setType('geopoint');
         return $me;
     }
+
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function GeopointMap($name)
     {
         $me = new self($name);
@@ -108,11 +194,44 @@ class Input extends ControlElement
         $me->setAppearance('maps');
         return $me;
     }
+
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
     public static function GeopointManual($name)
     {
         $me = new self($name);
         $me->setType('geopoint');
         $me->setAppearance('placement-map');
+        return $me;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
+    public static function GeoTrace($name)
+    {
+        $me = new self($name);
+        $me->setType('geotrace');
+        return $me;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Input
+     * @throws OException
+     */
+    public static function GeoShape($name)
+    {
+        $me = new self($name);
+        $me->setType('geoshape');
         return $me;
     }
 }
